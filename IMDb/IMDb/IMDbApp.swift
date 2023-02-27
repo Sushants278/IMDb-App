eacb5ace3c2b7a105342c341d18a7e789e66be58
Sushant S <32960414+Sushants278@users.noreply.github.com>
2023-02-27 15:33:13 +0100

    Created Folder Structure

---
+++ IMDb/IMDb/IMDbApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDbApp.swift
+//  IMDb
+//
+//  Created by Sushant Shinde on 27/02/23.
+//
+
+import SwiftUI
+
+@main
+struct IMDbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IMDb/IMDb/Interface/List/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IMDb
+//
+//  Created by Sushant Shinde on 27/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
