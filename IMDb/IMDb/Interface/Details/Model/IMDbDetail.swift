ab2d5009aaedf61a20f3fada481385eb43df3f69
Sushant S <32960414+Sushants278@users.noreply.github.com>
2023-02-28 17:08:09 +0100

    Fixed parsing issue

--- IMDb/IMDb/Interface/Details/Model/IMDbDetail.swift
+++ IMDb/IMDb/Interface/Details/Model/IMDbDetail.swift
@@ -9,16 +9,18 @@ import Foundation
 
 
 // MARK: - IMDBDetail
-struct IMDBDetail: Codable {
-    let title, year, rated, released: String
-    let runtime, genre, director, writer: String
-    let actors, plot, language, country: String
-    let awards: String
-    let poster: String
+struct IMDBDetail: Codable, Identifiable {
+    
+    let id = UUID()
+    let title, year, rated, released: String?
+    let runtime, genre, director, writer: String?
+    let actors, plot, language, country: String?
+    let awards: String?
+    let poster: String?
     let ratings: [Rating]
-    let metascore, imdbRating, imdbVotes, imdbID: String
-    let type, dvd, boxOffice, production: String
-    let website, response: String
+    let metascore, imdbRating, imdbVotes, imdbID: String?
+    let type, dvd, boxOffice, production: String?
+    let website, response: String?
 
     enum CodingKeys: String, CodingKey {
         case title = "Title"
@@ -54,5 +56,41 @@ struct Rating: Codable {
     enum CodingKeys: String, CodingKey {
         case source = "Source"
         case value = "Value"
+    }
+}
+
+
+import SwiftUI
+
+
+struct GenreView: View {
+    
+    let nums = [1, 2, 3,4,5]
+    
+    var body: some View {
+        // 1
+        ScrollView(.horizontal) {
+            
+            HStack {
+                // 2
+                ForEach(0...5, id: \.self) { index in
+                    
+                    getGenreView()
+                }
+            }
+        }
+    }
+    
+    func getGenreView() -> some View {
+        
+        return ZStack {
+            Text("IMDb")
+                .font(.system(size: 10, weight: .heavy, design: .default))
+                .padding(4)
+                .foregroundColor(.white)
+        }.background(Color.gray)
+            .opacity(0.8)
+            .padding(.leading, 6)
+        
     }
 }
