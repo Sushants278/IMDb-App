fa357fa20ad281770d989f75aba84f80662d7ef4
Sushant S <32960414+Sushants278@users.noreply.github.com>
2023-02-28 14:48:00 +0100

    Created IMDb Detail view

---
+++ IMDb/IMDb/Interface/Details/View/IMDbDetailView.swift
@@ -0,0 +1,23 @@
+//
+//  IMDbDetailView.swift
+//  IMDb
+//
+//  Created by Sushant Shinde on 28/02/23.
+//
+
+import SwiftUI
+
+struct IMDbDetailView: View {
+    
+    var body: some View {
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+/*
+struct IMDbDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        IMDbDetailView()
+    }
+}*/
